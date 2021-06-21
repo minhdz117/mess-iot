e8301420a6b7336f282a1427b49901303d81bd55
minh <minhdz117@gmail.com>
2021-06-21 15:38:25 +0900

    ok

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const fuj = require('./src-js/fujitsu')
+const { makeFujitsuPayload, addProntoMetadata, MODE, FANSPEED, SWING } = fuj
 const express = require('express')
 //const fs = require('fs')
 const bodyParser = require('body-parser')
@@ -8,28 +10,10 @@ const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-var ledIRcode=[{name:"1102472602",code:"on"},{name:"1102462402",code:"sleep"},{name:"1102478722",code:"off"},{name:"1102503202",code:"-"},{name:"1102470562",code:"+"}];
-var statusOfAir= {header:"28c60008087f900c",mid:"00000004",byte9:"",byte10:"",byte11:"",byte16:""}
-var state=[{name:"on",val:"0"},{name:"off",val:"8"}]
-var temp=[{name:"16C",val:"0"},
-          {name:"17C",val:"8"},
-          {name:"18C",val:"4"},
-          {name:"19C",val:"c"},
-          {name:"20C",val:"2"},
-          {name:"21C",val:"a"},
-          {name:"22C",val:"6"},
-          {name:"23C",val:"e"},
-          {name:"24C",val:"1"},
-          {name:"25C",val:"9"},
-          {name:"26C",val:"5"},
-          {name:"27C",val:"d"},
-          {name:"28C",val:"3"},
-          {name:"29C",val:"b"},
-          {name:"30C",val:"7"},]
-var mode=[{name:"cold"}]
+var ledIRcode=[{val:"1102472602",name:"on"},{val:"1102462402",name:"sleep"},{val:"1102478722",name:"off"},{val:"1102503202",name:"-"},{val:"1102470562",name:"+"}];
+
 //const privateKey = fs.readFileSync('./server.key', 'utf8');
 //const certificate = fs.readFileSync('./server.crt', 'utf8');
 //const credentials = { key: privateKey, cert: certificate };
@@ -101,11 +85,18 @@ function handleMessage(sender_psid, received_message) {
 
   // Check if the message contains text
   if (received_message.text) {
-      let country = ledIRcode.find(el => el.code ===received_message.text.split(" ")[1]);
-      if (country){
-        io.sockets.emit('event',country["name"])
+      if (received_message.text.split(" ")[0]=="led"){
+        let country = ledIRcode.find(el => el.name ===received_message.text.split(" ")[1]);
+        if (country){
+          io.sockets.emit('event',country["val"])
+        }
       }
+      if (received_message.text.split(" ")[0]=="air"){
+        let { tempC, mode, fanSpeed, swing, powerOn } = {tempC:"18",mode:"cool",fanSpeed:"auto",swing:"off",powerOn:"0"}
 
+        var payload = makeFujitsuPayload(tempC, MODE[mode.toLowerCase()], FANSPEED[fanSpeed.toLowerCase()], SWING[swing.toLowerCase()], powerOn)
+        console.log(payload)
+      }
     // Create the payload for a basic text message
     response = {
       "text": `You sent the message: "${received_message.text}"`
---
+++ src-js/fujitsu.js
@@ -0,0 +1,151 @@
+const checksum = require('./makeChecksum')
+
+const MODE = {
+  auto: 0x0,
+  cool: 0x8,
+  dry: 0x4,
+  fan_only: 0xc,
+  heat: 0x2
+}
+
+const FANSPEED = {
+  auto: 0x0,
+  high: 0x8,
+  med: 0x4,
+  low: 0xc,
+  quiet: 0x2
+}
+
+const SWING = {
+  off: 0x0,
+  horizontal: 0x4,
+  vertical: 0x8,
+  both: 0xc
+}
+
+const TEMPERATURE = {
+  16: 0x0,
+  17: 0x8,
+  18: 0x4,
+  19: 0xc,
+  20: 0x2,
+  21: 0xa,
+  22: 0x6,
+  23: 0xe,
+  24: 0x1,
+  25: 0x9,
+  26: 0x5,
+  27: 0xd,
+  28: 0x3,
+  29: 0xb,
+  30: 0x7
+}
+
+const PAYLOAD_OFF = [
+  0x28, 0xc6, 0x00, 0x08, 0x08, 0x40, 0xc0
+]
+
+function makePowerCode(powerOn) {
+  console.log(powerOn)
+  return powerOn > 0 ? 0x8 : 0x0
+}
+
+function makeTemperatureCode(tempC) {
+  // Clip between 16 and 30
+  var temp = Math.round(tempC)
+  temp = tempC > 30 ? 30 : tempC
+  temp = tempC < 16 ? 16 : tempC
+  return TEMPERATURE[temp]
+}
+
+function makeChecksumCode(payload) {
+  return checksum.makeChecksum(payload)
+}
+
+function concatBytes(a, b) {
+  return (a << 4) + b
+}
+
+function makeFujitsuPayload(tempC, mode, fanSpeed, swingMode, powerOn) {
+  // [1-8] Codes M1, M2, P, C1, C2, D, U1, U2
+  var payload = [0x28, 0xc6, 0x00, 0x08, 0x08, 0x7f, 0x90, 0x0c]
+
+  // [9] On/off + Temp
+  payload = [...payload, concatBytes(makePowerCode(powerOn), makeTemperatureCode(tempC))]
+  console.log(makePowerCode(powerOn))
+
+  // [10] Master mode + Timer mode
+  payload = [...payload, concatBytes(mode, 0x0)]
+
+  // [11] Fan speed + Swing mode
+  payload = [...payload, concatBytes(fanSpeed, swingMode)]
+
+  // [12] Timer off value (low)
+  payload = [...payload, 0x00]
+
+  // [13] Timer off value (high), New timer off (low)
+  payload = [...payload, 0x00]
+
+  // [14] Time on value (high)
+  payload = [...payload, 0x00]
+
+  // [15] U3
+  payload = [...payload, 0x04]
+
+  // [16] Checksum
+  payload = [...payload, makeChecksumCode(payload)]
+
+  return payload
+}
+
+function pad(num, size) {
+  var s = num + "";
+  while (s.length < size) s = "0" + s;
+  return s;
+}
+
+function addProntoMetadata(payload, frequency, leaderPair, trailerPair, onePair, zeroPair) {
+  for (var payloadBinary = "", i = 0; i < payload.length; i++) {
+    payloadBinary += pad(payload[i].toString(2), 8)
+  }
+  
+  var oneHex = pad(onePair[0].toString(16), 4) + pad(onePair[1].toString(16), 4)
+  var zeroHex = pad(zeroPair[0].toString(16), 4) + pad(zeroPair[1].toString(16), 4)
+  for (var prontoHex = "", i = 0; i < payloadBinary.length; i++) {
+    if (payloadBinary[i] === "1") {
+      prontoHex += oneHex
+    } else if (payloadBinary[i] === "0") {
+      prontoHex += zeroHex
+    }
+  }
+
+  var leaderHex = pad(leaderPair[0].toString(16), 4) + pad(leaderPair[1].toString(16), 4)
+  var trailerHex = pad(trailerPair[0].toString(16), 4) + pad(trailerPair[1].toString(16), 4) 
+
+  prontoHex = leaderHex + prontoHex + trailerHex
+
+  var burstLength = prontoHex.length/8
+  var burstLengthHex = pad(burstLength.toString(16), 4)
+
+  var freqConv = Math.round(1e6 / (frequency * 0.241246))
+  var freqConvHex = pad(freqConv.toString(16), 4)
+
+  prontoHex = "0000" + freqConvHex + "0000" + burstLengthHex + prontoHex
+
+  return prontoHex
+}
+
+function printArray(arr) {
+  console.log(`[${arr.join(', ')}]`)
+}
+
+if (require.main === module) {
+  var payload = makeFujitsuPayload(23, MODE.fan, FANSPEED.high, SWING.both, false)
+
+  printArray(payload.map(b => `'${b.toString(16)}'`))
+
+  var payloadBin = addProntoMetadata(payload, 39e3, [0x7C, 0x3E], [0x10, 0x130], [0x10, 0x2E], [0x10, 0x10])
+  console.log(payloadBin)
+}
+
+module.exports = {makeFujitsuPayload, addProntoMetadata, MODE, FANSPEED, SWING}
